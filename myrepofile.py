535c04e547d107e010fba9b05ae7102752990623
mohammad <Mohammadalthaf@gmail.com>
2024-07-22 22:28:49 +0530

    message

--- myrepofile.py
+++ myrepofile.py
@@ -1,10 +1,16 @@
-"""print("this is my first repository")
+'''print("this is my first repository")
 
 
 a = "hello,boss"
-print(len(a))"""
+print(len(a))'''
 
-#write a program to read integer and convert it into float?
+'''#write a program to read integer and convert it into float?
 
 b=20
-print(float(b))
+print(float(b))'''
+'''
+#write a python program insert elemnt at end of an array?
+'''
+nums = [1,2,3,4,5]
+nums.insert(5,6)
+print(nums)
