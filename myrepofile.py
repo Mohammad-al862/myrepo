35007efbb966927bc4dc59265403819081271f2b
mohammad <Mohammadalthaf@gmail.com>
2024-07-22 17:11:57 +0530

    message

--- myrepofile.py
+++ myrepofile.py
@@ -1,5 +1,10 @@
-print("this is my first repository")
+"""print("this is my first repository")
 
 
 a = "hello,boss"
-print(len(a))
+print(len(a))"""
+
+#write a program to read integer and convert it into float?
+
+b=20
+print(float(b))
