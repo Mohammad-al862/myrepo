7dd4d37aec93bcdbd3f1cf3d98d88ca729d5fabb
mohammad <Mohammadalthaf@gmail.com>
2024-07-23 10:13:14 +0530

    forth q

---
+++ sub4.py
@@ -0,0 +1,11 @@
+#write a python program to find large and small numbers in a list?
+
+numbers = [1,2,3,4,5,6,7,8,9]
+largest_number = max(numbers)
+smallest_number = min(numbers)
+print("Largest number:",largest_number)
+print("Smallest number:",smallest_number)
+
+
+
+
