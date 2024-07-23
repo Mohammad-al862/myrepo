fefdc235dd29e86ebbd5deaf3aed98446f671f07
mohammad <Mohammadalthaf@gmail.com>
2024-07-23 17:44:26 +0530

    sub6.py

--- sub6.py
+++ sub6.py
@@ -1,7 +1,11 @@
-#write a python program reverse the list an two ways?
+'''#write a python program reverse the list an two ways?
 l1 = [1,2,3,4,5]
 l2 = []
 for item in reversed(l1):
     l2.append(item)
-    print(l2)
-    
+    print(l2)'''
+     
+#write a python program to find height frequency element in list?
+
+nums = [1,2,3,4,5,5,4,6,5,7,5]
+print(nums.count(5))
