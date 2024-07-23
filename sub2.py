e065d11a2e738cc44ae42f0a27f34c7ef666eee9
mohammad <Mohammadalthaf@gmail.com>
2024-07-23 08:54:14 +0530

    second question

---
+++ sub2.py
@@ -0,0 +1,14 @@
+#python program to create list?
+
+l = [1,2,3,4]
+print(type(l))
+
+l1 = eval(input("enter your list :"))
+print(type(l1))
+
+l2 = input("enter your list :")
+print(type(l2))
+ 
+l3 = "20,30,40,50"
+print(l3.split(","))
+ 
