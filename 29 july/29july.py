77397d0ad0418f6f6f11ebc27ee1176b0b8fc9a5
mohammad <Mohammadalthaf@gmail.com>
2024-07-29 23:06:01 +0530

    29 july

---
+++ 29 july/29july.py
@@ -0,0 +1,31 @@
+#how do you create a empty tuple in python?
+
+empty_tuple = ()
+print(type(empty_tuple))
+print(empty_tuple)
+
+#write a python program to unpack tuple into several variable?
+
+
+#write a python program to add an element to a tuple?
+
+
+domains = ("action","comedy","horrer")
+d = list(domains)
+d[2] = "film"
+domains = tuple(d)
+print(domains)
+
+#write a python program to convert tuple  into string?
+
+t1 = ("fresh","mango","juice")
+str1 =""
+for ele in t1:
+    str1+=ele
+print(str1)
+print(type(str1))
+
+
+
+#write a python program to find most frequent element in the tuple?
+ 
